a2c22c56dbc4833ef445d3416902dcf43277a997
matheuscamarques <matheuscamarques@gmail.com>
2022-03-14 21:10:03 -0300

    Letter aplication version by Matheus C Marques

--- projetos/letter/lib/letter.ex
+++ projetos/letter/lib/letter.ex
@@ -3,10 +3,39 @@ defmodule Letter do
   Documentação para o módulo `Letter`.
   """
 
-  @word_of_the_day "TOUCA"
+  def compare(input, palavra) do
+    compare(input,palavra,[],palavra)
+  end
 
-  def guess(user_guess) do
-    user_guess == @word_of_the_day
+
+  defp compare([], [], saida, _) do
+    saida
+  end
+
+  defp compare([a | b], [c | d] , saida, palavra) when a == c do
+      compare(b, d, saida ++ [1], palavra)
+  end
+
+  defp compare([a | b], [c | d] , saida , palavra) when a != c do
+      case exists?(a,palavra) do
+        true ->
+          compare(b, d, saida ++ [0], palavra)
+        false ->
+          compare(b, d, saida ++ [2], palavra)
+      end
+  end
+
+  defp exists?(_, []) do
+    false
+  end
+
+  defp exists?(elemento,[a | b]) do
+    case [elemento] == a do
+      true ->
+        true
+      false ->
+        exists?(elemento,b)
+    end
   end
 
 end
